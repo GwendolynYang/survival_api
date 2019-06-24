ed582d6c25a36001c084727bb222b57062f10113
GwendolynYang <shining.yg@gmail.com>
2019-06-24 15:25:23 -0400

    first submission

--- survival_api/urls.py
+++ survival_api/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+
+from predictor.views import predictor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('predictor/', include('predictor.urls')),
+]
+
+urlpatterns += [
+    url(r'^weblink/$', predictor, name='weblink'),
 ]
