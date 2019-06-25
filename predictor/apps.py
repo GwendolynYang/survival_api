296e71c619c426a32a529b51e825a67156846a00
GwendolynYang <shining.yg@gmail.com>
2019-06-25 00:43:37 -0400

    first update. General framework done.

--- predictor/apps.py
+++ predictor/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+import pandas as pd
 
 class PredictorConfig(AppConfig):
     name = 'predictor'
+
+    def ready(self):
+
+        global feat_all
+
+        feat_all = pd.read_csv('../../survival_api_data/feat_all.csv')
--- predictor/views.py
+++ predictor/views.py
@@ -1,14 +1,61 @@
-from django.http import HttpResponse
+
 
 # Create your views here. example from https://docs.djangoproject.com/en/2.2/intro/tutorial01/
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 
-
+from django.http import HttpResponse
+import json
 import pandas as pd
 import numpy as np
-
 from lifelines import CoxPHFitter
 from lifelines.utils import median_survival_times, qth_survival_times
 
+from scraper.views import houseScraper
+from cleansor.views import featPrep
+
 def predictor(request):
+    for_sale_house = request.GET['for_sale_house']
+
+    house_info = houseScraper(for_sale_house)
+    house_feat = featPrep(house_info)
+
+    # training model with feat_all
+    if 'address' in feat_all.columns:
+        feat_all.drop(['address'], axis=1, inplace=True)
+
+    # fitting model on time
+    cph_time = CoxPHFitter()
+    cph_time.fit(feat_all, duration_col='days', event_col='sold', show_progress=True)
+
+    # prediction
+    # # TODO: each house prediction. tested on jupyter
+    uncon_time = cph_time.predict_survival_function(house_feat)
+    time_life = uncon_time.apply(lambda c: (c / c.loc[feat_all.loc[c.name, 'sold']]).clip_upper(1))
+
+    time_pred_50 = median_survival_times(time_life)
+    time_pred_75 = qth_survival_times(0.25, time_life)
+
+    # fitting model on discount
+    cph_off = CoxPHFitter()
+    cph_off.fit(feat_all, duration_col='discount', event_col='sold', show_progress=True)
+
+    # prediction on time
+    # # TODO: each house prediction. tested on jupyter
+    uncon_off = cph_off.predict_survival_function(house_feat)
+    # off_life = uncon_off.apply(lambda c: (c / c.loc[feat_all.loc[c.name, 'sold']]).clip_upper(1))
+
+    off_pred_50 = median_survival_times(uncon_off)
+    off_pred_75 = qth_survival_times(0.25, uncon_off)
+
+    response_dict = {
+        'time_pred_50': time_pred_50,
+        'time_pred_75': time_pred_75,
+
+        'off_pred_50' : off_pred_50,
+        'off_pred_75' : off_pred_75,
+    }
+
+    response = json.dumps(response_dict)
+
+    return HttpResponse(response)
