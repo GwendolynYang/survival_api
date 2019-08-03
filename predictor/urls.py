bd8fde891d6267be16d12493925707e9b2ea2132
GwendolynYang <shining.yg@gmail.com>
2019-08-03 12:58:45 -0400

    last

--- predictor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-from django.conf.urls import url
-from predictor.views import predictor
-
-urlpatterns = [
-    url(' ', predictor, name='weblink'),
-]
--- survival_api/settings/base.py
+++ survival_api/settings/base.py
@@ -37,16 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'predictor.apps.PredictorConfig',
+    'cleaner.apps.CleanerConfig',
+    'scraper.apps.ScraperConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'survival_api.urls'
@@ -118,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOW_CREDENTIALS = False
+
+CORS_ORIGIN_WHITELIST = ['https://www.trulia.com']
